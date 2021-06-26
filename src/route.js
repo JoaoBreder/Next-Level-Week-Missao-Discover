408c4d6d9bad5faef07e600a07a18ee033d06f7b
João Breder <joao.g.breder@gmail.com>
2021-06-26 19:37:22 -0300

    Aula 05 - Aprofundando no SQL

--- src/route.js
+++ src/route.js
@@ -3,12 +3,17 @@ const QuestionController = require('./controllers/question-controller')
 const RoomController = require('./controllers/room-controller')
 const route = express.Router()
 
+//Rotas relacionadas a enter-room e a create-pass
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room/:room', (req, res) => res.render("room"))
-
-route.post('/question/:room/:question/:action', QuestionController.index)
+//Rotas relacionadas a sala
 route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
+
+//Rotas relacionadas as questões
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = route
